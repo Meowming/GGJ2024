46251771d54ba303490ff8e5be505d3766f861e3
SakuraMo <sakuramopoi@gmail.com>
2024-01-27 13:26:59 -0800

    Update StartPanel.cs

--- Assets/Scripts/StartScenes/UI/StartPanel.cs
+++ Assets/Scripts/StartScenes/UI/StartPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartPanel : BasePanel
@@ -12,7 +13,9 @@ public class StartPanel : BasePanel
     {
         StartButton.onClick.AddListener(() =>
         {
+            UIManager.Instance.HidePanel<StartPanel>();
 
+            SceneManager.LoadScene("BeginScenes");
         });
 
         QuitButton.onClick.AddListener(() =>
