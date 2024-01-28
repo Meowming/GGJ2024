c7f357e6c208853ce2c0b56c15b1f105dca6938a
SakuraMo <sakuramopoi@gmail.com>
2024-01-27 20:30:38 -0800

    StartPanel2

---
+++ Assets/Scripts/Data/MusicData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicData
+{
+    //背景音乐和音效的大小
+    public float musicValue = 0.2f;
+    public float soundValue = 0.2f;
+
+    ////背景音乐和音效的开关
+    //public bool musicOpen = true;
+    //public bool soundOpen = true;
+}
