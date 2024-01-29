7fbbeb3e0d67a2faec81ce1b9effd80f2c3a3918
SakuraMo <sakuramopoi@gmail.com>
2024-01-28 18:20:46 -0800

    Update StartPanel.cs

--- Assets/Scripts/Scenes/UI/StartPanel.cs
+++ Assets/Scripts/Scenes/UI/StartPanel.cs
@@ -11,8 +11,6 @@ public class StartPanel : BasePanel
     public Button SettingButton;
 
     public Button QuitButton;
-
-    public Animation animation;
     public override void Init()
     {
         StartButton.onClick.AddListener(() =>
